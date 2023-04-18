c7b334ea25ea21dfc07dd8d5da280e9c295f2715
OrBonker <94792373+OrBonker@users.noreply.github.com>
2023-04-18 12:46:44 +0300

    login and register back

--- app/auth.py
+++ app/auth.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db   ##means from __init__.py import db
-from flask_login import login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,7 +18,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('views.equipment'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
--- app/models.py
+++ app/models.py
@@ -2,10 +2,16 @@ from . import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    #__tablename__ = 'users'
-    email = db.Column(db.String(150), unique=True, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     phone = db.Column(db.String(10))
     name = db.Column(db.String(150))
-    phone = db.Column(db.Integer)
+    
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
     
