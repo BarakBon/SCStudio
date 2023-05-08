223e5096196b03520213fd2eace2baee20cfc40c
BarakBon <bar14ak@gmail.com>
2023-05-08 12:44:46 +0300

    borrow model and database type and name changes

--- app/models.py
+++ app/models.py
@@ -4,23 +4,29 @@ from flask_login import UserMixin
 
 class Equipment(db.Model, UserMixin):
     Type= db.Column(db.String(10))
-    model=db.Column(db.String(150))
-    Serial_Number= db.Column(db.String(150), primary_key=True)
-    Available = db.Column(db.String(150))
-    Time = db.Column(db.String(150))
+    model=db.Column(db.String(50))
+    serial_number= db.Column(db.String(50), primary_key=True)
+    available = db.Column(db.String(15))
+    max_time = db.Column(db.Integer)
     
     
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     userType = db.Column(db.String(8))
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(30))
     phone = db.Column(db.String(10))
-    name = db.Column(db.String(150))
+    name = db.Column(db.String(50))
 
 
+class Borrow(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    borrower = db.Column(db.Integer, db.ForeignKey('user.id'))
+    aq_serial = db.Column(db.String(50), db.ForeignKey('equipment.serial_number'))
+    borrow_date = db.Column(db.String(10))
+    return_date = db.Column(db.String(10))
 
-    
+
 '''from . import login_manager
 
 @login_manager.user_loader
