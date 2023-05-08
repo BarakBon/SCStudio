e3e4ce9495bd10d04fc29fbe4ee4f27651c3db26
BarakBon <bar14ak@gmail.com>
2023-05-08 15:16:27 +0300

    fixed password

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     userType = db.Column(db.String(8))
     email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(30))
+    password = db.Column(db.String(100))
     phone = db.Column(db.String(10))
     name = db.Column(db.String(50))
 
